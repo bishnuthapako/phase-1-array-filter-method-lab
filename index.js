bf209204a966d64d5dd68d22e5bcb6f41b218979
Bishnu <bishnuthapako@gmail.com>
2022-04-07 21:12:43 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution here
+
+
+
+function findMatching(drivers, string){
+  const matchDrive = drivers.filter(function(value){
+    if(value.toLowerCase() === string.toLowerCase()){
+      return true
+    } else {
+      return false
+    }
+
+  })
+return matchDrive
+  
+}
+
+function fuzzyMatch(drivers, string){
+  const matchingDriver = drivers.filter(function(drivers){
+    if(string === drivers.slice(0,2)){
+      return(drivers);
+    } else {
+      return drivers === string;
+    }
+
+  })
+  return matchingDriver
+
+}
+
+
+function matchName(drivers, string){
+  debugger;
+  const objectDriver = drivers.filter(function(string){
+    if(string === drivers[0]){
+      return drivers
+
+    } else {
+      return string === drivers[4]
+    }
+  })
+  return objectDriver
+}
+matchName(drivers)
